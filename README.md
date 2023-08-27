29f55b62b7a6806287921ed2d5f7642b8ee822c4
Jonathan Santos <jonathan.tecsantos@gmail.com>
2023-08-27 18:20:13 -0300

    Ajustes de layout e add RecyclerView

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # app_kotlin
+
+Layout:
+
+match_parent: O limite é o componente pai
+wrap_parent: O componente se ajustar até o seu tamanho
+app:layout_constraintStart_toStartOf="parent": definição de inicio do layout do componente
+app:layout_constraintTop_toBottomOf="@id/descricao": definição de inicio do layout do componente
+
+margin:
+
+android:layout_margin="16dp"
+dp = densindade de pixel (o layout calcula a quantidade de pixel necessária em cada tela para chegar no dp estabelecido)
+
+recyclerView - lista dinamica (groupView)
--- app/src/main/java/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/com/alura/orgs/MainActivity.kt
@@ -5,12 +5,22 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+//        val nome = findViewById<TextView>(R.id.nome)
+//        val descricao = findViewById<TextView>(R.id.descricao)
+//        val valor = findViewById<TextView>(R.id.valor)
+//
+//        nome.text = "Cesta de frutas"
+//        descricao.text = "Manga"
+//        valor.text = "4,88"
+
 
         Toast.makeText(this, "Oi bb :D", Toast.LENGTH_SHORT).show()
     }
